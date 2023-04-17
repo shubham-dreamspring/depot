a5a4abfb75429e63eff5e49c903b79c0be468009
shubham-dreamspring <sjain@dreamspring.org>
2023-04-17 11:14:27 +0530

    Done a lot of stuff

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    return if line_items.empty?
+
+    errors.add(:base, 'Line Items present')
+    throw :abort
+  end
+
   validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -42,6 +42,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
                                                       price: @product.price, title: @title } }
     assert_redirected_to product_url(@product)
   end
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+    assert_redirected_to products_url
+  end
 
   test 'should destroy product' do
     assert_difference('Product.count', -1) do
